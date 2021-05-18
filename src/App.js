7cda077106136862d7b321eb9ee53ecb1881569a
Lalngaisang Touthang <touthanglalngaisang@gmail.com>
2021-05-18 10:09:23 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          This is Lalngaisang Touthang's website.
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
